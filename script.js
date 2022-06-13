06306bcf120066bcb0b5f4f9b6a78784ec7bedb8
Olivia Lammam <olivia.l7475@gmail.com>
2022-06-12 21:30:06 -0700

    updated

--- script.js
+++ script.js
@@ -30,10 +30,29 @@ var setCharacterLimit = function () {
   criteria.characterLimit = window.prompt("What's your desired character limit?\n[Length must be at least 8 characters but no more than 128 characters]");
   criteria.characterLimit = Number(criteria.characterLimit);
   if (typeof criteria.characterLimit === typeof 0 && criteria.characterLimit >= 8 && criteria.characterLimit <= 128) {
-    typeof criteria.characterLimit
+    window.alert("You set the character limit to: " + criteria.characterLimit); 
+  } else {
+    window.alert("Please enter a number valued between 8 and 128");
     setCharacterLimit();
   }
 };
+
+var declareOtherCriteria = function() {
+  criteria.lowerCase = window.confirm("Do you want a Lower Case password?");
+  criteria.upperCase = window.confirm("Do you want a Upper Case password?");
+  criteria.numbers = window.confirm("Do you want Numbers in your password?");
+  criteria.specialCharacters = window.confirm("Do you want Special Characters in your password?");
+}
+
+var promptCriteria = function () {
+  criteria.promptConfirm = window.confirm("Would you like to generate a password?");
+  if (criteria.promptConfirm) {
+    setCharacterLimit();
+    declareOtherCriteria();
+  } else {
+  };
+}
+
 
 
 
