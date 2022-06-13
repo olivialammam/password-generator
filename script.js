5e554456ee4363242075e0228429c778f8ac7d0c
Olivia Lammam <olivia.l7475@gmail.com>
2022-06-12 21:19:19 -0700

    added variables

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 // Assignment code here
+var criteria = {
+  promptConfirm: false,
+  characterLimit: 8,
+  upperCase: true,
+  lowerCase: true,
+  numbers: true,
+  specialCharacters: true,
+};
+
+
+var characters = {
+  letter: "abcdefghijklmnopqrstuvwxyz",
+  number: "0123456789",
+  special: "!#$%&()*+,-./:;<=>?@^_`{|}~",
+};
+
+
+var randomNum = function (min,max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
+var randomCharacter = function (charactersProperty) {
+  randomPickedCharacter = charactersProperty.charAt(randomNum(0,charactersProperty.length));
+  return randomPickedCharacter;
+};
+
 
 
 // Get references to the #generate element
